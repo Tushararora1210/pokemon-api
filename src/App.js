5585f1c040224c94a68a9b14014353174c99b1b0
Tushar Arora <Tushararora@gmail.com>
2021-01-25 18:30:13 +0530

    Pokemon API Fetched and functionalities implemented

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import Loading from "./Components/loading";
+import Pokemoncard from "./Components/pokemoncard";
+import './App.css';
+import React,{useState,useEffect} from "react";
+import {getAllPokemon,getpokemon} from "./functions/function";
+
+function App() {
+  const [pokemonlist,setpokemonlist]=useState([]);
+  const [nextbuttonlink,setnextbuttonlink]=useState("");
+  const [prevbuttonlink,setprevbuttonlink]=useState("");
+  const [currpageurl,setcurrpageurl]=useState("https://pokeapi.co/api/v2/pokemon");
+  const [loading,setloading]=useState(true);
+  useEffect(()=>{
+    async function fetchdata()
+    {
+    setloading(true);
+    
+  
+    let response=await getAllPokemon(currpageurl);
+    console.log(response);
+    setnextbuttonlink(response.next)
+    setprevbuttonlink(response.previous)
+    let pokemondetailsarray=await Promise.all(response.results.map(async (pokemon)=>{
+     let eachpokemon=await getpokemon(pokemon.url);
+     //console.log(eachpokemon);
+     return eachpokemon;
+    }))
+    setpokemonlist(pokemondetailsarray);
+    console.log(pokemondetailsarray);
+    setloading(false);
+
+    }
+    fetchdata();
+},[currpageurl])
+  
+  
+
+
+  if(loading)
+  {
+    return(
+      <div>
+        <Loading/>
+      </div>
+    )
+    }
+    return (
+    <div className="App">
+      <h1>Here is List of All Pokemons</h1>
+      <h2>Made By Tushar</h2>
+      <h3>Using PokemonAPI</h3>
+      <h6>Fetched from <a href={currpageurl}>HERE</a> </h6>
+      <div className="ShowPokemons">
+      {pokemonlist.map((pokemon)=>{
+        return(
+          <div>
+            <Pokemoncard pokemon={pokemon}/>
+          </div>
+        )
+      })}
+      </div>
+      <div className="buttons">
+        <button onClick={()=>{
+          console.log(prevbuttonlink);
+          if(prevbuttonlink)
+          {
+            
+            setcurrpageurl(prevbuttonlink);
+          }
+        }}>Prev</button>
+        <button onClick={()=>{
+          if(nextbuttonlink)
+          {
+           
+            setcurrpageurl(nextbuttonlink);
+          }
+        }}>Next</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
